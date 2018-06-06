5b043083205583d4aabbffbeac971d500f74d9e3
4ever <pierreandreisortega@gmail.com>
2018-06-05 21:54:32 -0700

    Adding first commits, cleaning up

--- src/App.js
+++ src/App.js
@@ -1,19 +1,21 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Button } from "reactstrap";
+import Header from "./Header";
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <React.Fragment>
+        <Header />
+        <article className="container text-center py-5">
+          <h1 className="pt-5">VAINGLORY ICONS FONT</h1>
+          <small className="d-block half-opacity pb-3">REFERENCE</small>
+
+          <Button color="warning" size="lg">
+            HOW TO USE
+          </Button>
+        </article>
+      </React.Fragment>
     );
   }
 }
---
+++ src/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+export default class Header extends React.Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <nav className="navbar navbar-expand-md navbar-dark fixed-top justify-content-between header-and-footer">
+            <a href="/" className="navbar-brand">VAINGLORY TOOLS</a>
+          </nav>
+        </header>
+      </div>
+    )
+  }
+};
